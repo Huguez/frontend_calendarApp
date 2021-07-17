c9293e2f05f9577b8c2d75d148309ee7516ad3e8
Huguez <carlos.huguez@gmail.com>
2021-07-16 20:43:22 -0700

    Contenido del EventModal

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -2,6 +2,9 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal'
 import './CalendarModal.css'
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment';
+
 
 const customStyles = {
     content: {
@@ -19,8 +22,7 @@ Modal.setAppElement('#root');
 
 
 export const CalendarModal = () => {
-    // let subtitle
-
+    
     const [modalIsOpen, setIsOpen] = useState( false );
 
     function openModal() {
@@ -36,13 +38,26 @@ export const CalendarModal = () => {
         setIsOpen(false);
         // console.log("cerrando...")
     }
+
     
+    const [startDate, setStartDate] = useState( moment().minute(0).second(0).toDate() )
+    
+    const handleStartDateChange = ( e ) => {
+        setStartDate( e )
+    }
+    
+    const [endDate, setEndDate] = useState( moment().minute(0).second(0).add( 1,'hours' ).toDate() )
+    
+    const handleEndDateChange = ( e ) => {
+        setEndDate( e )
+    }
+
     return (
         <div>
             <button onClick={openModal}>Open Modal</button>
 
             <Modal 
-                isOpen={modalIsOpen}
+                isOpen={ true }
                 onAfterOpen={afterOpenModal}
                 onRequestClose={closeModal}
                 style={customStyles}
@@ -50,15 +65,57 @@ export const CalendarModal = () => {
                 className="modal"
                 overlayClassName="modal-fondo"
             >
-                <h2>Hello</h2>
-                <button onClick={closeModal}>close</button>
-                <div>I am a modal</div>
-                <form>
-                    <input />
-                    <button>tab navigation</button>
-                    <button>stays</button>
-                    <button>inside</button>
-                    <button>the modal</button>
+                <h1> Nuevo evento </h1>
+                <hr />
+                <form className="container">
+                    <div className="form-group">
+                        <label>Fecha y hora inicio</label>
+                        {/* <input className="form-control" placeholder="Fecha inicio" /> */}
+                        <DateTimePicker className="form-control"  
+                            value={ startDate }
+                            onChange={ handleStartDateChange} />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Fecha y hora fin</label>
+                        <DateTimePicker className="form-control"  
+                            value={ endDate }
+                            minDate={ startDate }
+                            onChange={ handleEndDateChange } />
+                    </div>
+
+                    <hr />
+                    <div className="form-group">
+                        <label>Titulo y notas</label>
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            placeholder="Título del evento"
+                            name="title"
+                            autoComplete="off"
+                        />
+                        <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                    </div>
+
+                    <div className="form-group">
+                        <textarea 
+                            type="text" 
+                            className="form-control"
+                            placeholder="Notas"
+                            rows="5"
+                            name="notes"
+                        ></textarea>
+                        <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-outline-primary btn-block"
+                    >
+                        <i className="far fa-save"></i>
+                        <span> Guardar</span>
+                    </button>
+
                 </form>
             </Modal>
         </div>
